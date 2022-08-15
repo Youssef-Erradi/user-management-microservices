da7f212575398dbe1156dcb1edb3bdefbc54ed1c
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-08-15 21:10:02 +0100

    add commands classes

---
+++ user-command-api/src/main/java/user/cmd/api/commands/RegisterUserCommand.java
@@ -0,0 +1,15 @@
+package user.cmd.api.commands;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import lombok.Builder;
+import lombok.Data;
+import user.core.models.User;
+
+@Data @Builder
+public class RegisterUserCommand {
+	@TargetAggregateIdentifier
+	private String id;
+	private User user;
+
+}
---
+++ user-command-api/src/main/java/user/cmd/api/commands/RemoveUserCommand.java
@@ -0,0 +1,12 @@
+package user.cmd.api.commands;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+import lombok.Data;
+
+@Data
+public class RemoveUserCommand {
+	@TargetAggregateIdentifier
+	private String id;
+
+}
