056f8ff40a76bed278068ababcc69041cbf5fd05
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-09-13 00:46:17 +0100

    add AllArgsConstructor

--- user-command-api/src/main/java/user/cmd/api/commands/RemoveUserCommand.java
+++ user-command-api/src/main/java/user/cmd/api/commands/RemoveUserCommand.java
@@ -2,9 +2,11 @@ package user.cmd.api.commands;
 
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RemoveUserCommand {
 	@TargetAggregateIdentifier
 	private String id;
