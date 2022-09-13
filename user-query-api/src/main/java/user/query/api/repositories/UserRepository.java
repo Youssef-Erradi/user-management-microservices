eb97969145761c6ea03e351cd8957a327f94d0f1
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-09-13 01:22:37 +0100

    add searchByFilter Query method

--- user-query-api/src/main/java/user/query/api/repositories/UserRepository.java
+++ user-query-api/src/main/java/user/query/api/repositories/UserRepository.java
@@ -1,9 +1,21 @@
 package user.query.api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import user.core.models.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
-
+	@Query("""
+			{ '$or' : [
+				{ 'firstname' : { $regex : ?0, $options : '1' } },
+				{ 'lastname' : { $regex : ?0, $options : '1' } },
+				{ 'email' : { $regex : ?0, $options : '1' } },
+				{ 'account.username' : { $regex : ?0, $options : '1' }}
+			]}
+			""")
+	List<User> searchByFilter(String filter);
+	
 }
