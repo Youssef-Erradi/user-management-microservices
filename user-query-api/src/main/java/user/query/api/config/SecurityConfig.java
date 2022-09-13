a8b08427fc24b12b8cf382f886169d5bcc61dd53
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-09-13 03:50:55 +0100

    add security config class

---
+++ user-query-api/src/main/java/user/query/api/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package user.query.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		http.authorizeRequests().anyRequest().permitAll()
+			.and()
+			.csrf().disable();
+        return http.build();
+    }
+}
