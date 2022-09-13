384ab79e0af1ccb445db8fff8e0dfd7643551794
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-09-13 03:50:24 +0100

    replace record with class

--- user-query-api/src/main/java/user/query/api/dto/UserLookupResponse.java
+++ user-query-api/src/main/java/user/query/api/dto/UserLookupResponse.java
@@ -1,9 +1,21 @@
 package user.query.api.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import user.core.models.User;
 
-public record UserLookupResponse(List<User> users) {
+@Data
+@AllArgsConstructor
+public class UserLookupResponse {
+	private List<User> users;
+	
+	public UserLookupResponse(User user) {
+		users = new ArrayList<>();
+		if(user != null)
+			users.add(user);
+	}
 
 }
